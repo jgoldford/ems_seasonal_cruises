5c2687bf5c031064267b06c870a1b62094cec151
jgoldford <joshuagoldford@vpn1-dhcp65-134.bu.edu>
2018-06-23 17:02:28 -0400

    added new analysis of TARA dataset

---
+++ analysis/main_01_June24_2018_TARA.m
@@ -0,0 +1,40 @@
+% analysis of TARA oceans data (at the pathway level)
+% June-23-2018
+% Joshua Goldford
+
+clear
+% load the data
+load('../TARA/tara_data_parsed.mat')
+
+% analyze modules first
+[~,i,j]=intersect(meta.samples_for_analysis.Samplename,meta.pathways.sample);
+
+
+% construct normalized matrix of pathway abundances
+samples = meta.samples_for_analysis(i,:);
+pathway_matrix = meta.pathways.P(:,j);
+pathway_matrix = cell2mat(cellfun(@(x) x./sum(x), num2cell(pathway_matrix,1),'uni',0));
+
+% for each pathway, compute the pearson correlation coefficient
+for i = 1:length(meta.pathways.id)
+    [r(i),p(i)]=corr(samples.Actualdepth,pathway_matrix(i,:)');
+end
+
+% find significant pathways using an FDR q-value of 0.05 (Benjamini &
+% Hochberg procedure)
+[h,~,~,padj]=fdr_bh(p,0.05);
+pathways = meta.pathways.id(h);
+[r_sorted,i]= sort(r(h),'descend');
+
+
+% make a figure showing the pathways and their correlation with depth
+barh(r_sorted);
+set(gca,'YTick',1:length(r_sorted));
+set(gca,'YTickLabel',pathways(i));
+set(gca,'Fontsize',6.8);
+ylabel('significant pathways')
+xlabel('pearsons correlation coefficient')
+
+
+
+
